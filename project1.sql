5f4c48e2a19375f555dde498b9eaf3322e50458a
GermanMunguia <44210947+GermanMunguia@users.noreply.github.com>
2020-02-08 19:00:05 -0500

    Add files via upload

---
+++ project1.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE login (
+username VARCHAR(20) PRIMARY KEY,
+password VARCHAR(20),
+socialS VARCHAR(4),
+email VARCHAR(20),
+ DoB date
+); 
+
+-- ALTER TABLE login
+-- ;
+
+-- DELETE FROM login 
+-- WHERE username = ''; 
+
+-- DELETE FROM login 
+-- WHERE DoB < '2001-01-01'; 
+
+-- INSERT INTO login VALUES(
+-- 'testy1',
+-- 'passw',
+-- '1234',
+--  'g',
+-- '2000-2-28'
+-- );
+
+-- SELECT * FROM login; 
